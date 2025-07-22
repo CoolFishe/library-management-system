a172a451c75ffcf52b7864ffa19bc9a57afa1691
CoolFishe <luckyzzbb@163.com>
2025-07-22 17:00:55 +0800

    update

---
+++ src/test/java/com/zz/librarymanagement/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zz.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
